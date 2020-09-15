29fb99c0e623fe4b8e94fc9591ec2a654a17843b
Doooooonglin <71182884+Doooooonglin@users.noreply.github.com>
2020-09-14 22:56:13 -0400

    Update Program.cs

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using calculatorEnging.Dll
 
 namespace SimpleCalculator
 {
@@ -19,15 +20,31 @@ namespace SimpleCalculator
 
                 //Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
+         
 
                 double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
                 double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
                 string operation = Console.ReadLine();
 
+                static double CheckNum(string num)
+                try
+                {
+                    double firstNumber = double.Parse(num);
+                    return firstNumber;
+                    double secondNumber = double.Parse(num);
+                    return secondNumber;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You should enter a number, not a symbol, words or letters：");
+                    string operation = Console.ReadLine();
+                    return CheckNum(str);
+                }
+                Console.WriteLine("enter '+' or 'plus' to indicate your operation, such as: "+firstNumber+operation+secondNumber) ;
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
                 Console.WriteLine(myResult);
-
+                
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
